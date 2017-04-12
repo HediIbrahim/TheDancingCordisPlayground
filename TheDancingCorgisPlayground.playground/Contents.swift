1102bb2e54a4cc1c0cea5cdd5ea4ffefe1ca3004
Hedi <HediIbrahim@users.noreply.github.com>
2017-04-12 15:30:26 -0400

    Add files via upload

---
+++ TheDancingCorgisPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+print("Hello world")
+print("My favorite color is Light pink")
+print(str)
+
+str = "I love corgis"
+print(str)
+
+6+4
+9-0
+
+var h = 11
+var z = 47
+var ah = 49
+var am = 10
+
+var result = ah+am*h/z-am
+
+result = ah+h
+result = ah*am
+
+result+=am
+result*=am
